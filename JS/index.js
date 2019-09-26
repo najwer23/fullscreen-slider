ff2beb07b312fe5e92f76457cdf631cae9afb6a9
Mariusz Najwer <najwer23@gmail.com>
2019-09-26 18:37:48 +0200

    finished slider

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,36 @@
 
-let numerZdjecia = 0;
-let zdjecia = ["pic1","pic2", "pic3"];
+let idPicture = 0;
+let pictures = ["pic1","pic2", "pic3", "pic4", "pic5", "pic6", "pic7", "pic8", "pic9", "pic10", "pic11"];
+
+let idPicture2Show = 1;
+let formatPicture = ".jpg";
+let pictureName = pictures[0] + formatPicture;
+
+function start() {
+    showDescription();    
+};
+
+function showDescription() {
+    let d = document.getElementById("description"); 
+    d.innerHTML = "(" + idPicture2Show + "/" + pictures.length + ") " + pictureName;
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode === 37) {
+        prev();
+    }
+    
+    if (e.keyCode === 39) {
+        next();
+    }
+}
+
+
 
 function next () {
 
@@ -13,23 +43,27 @@ function next () {
     f1.classList.remove("slideInRight");
     f2.classList.remove("slideOutLeft");
 
-    if (numerZdjecia < 0) {
-        numerZdjecia = zdjecia.length + (numerZdjecia % zdjecia.length)
+    if (idPicture < 0) {
+        idPicture = pictures.length + (idPicture % pictures.length);
     } 
 
     f1.classList.add("hide"); 
-    f1.setAttribute("style", "background-image: url('IMG/" + zdjecia[(numerZdjecia+1)%zdjecia.length] + ".jpg')")
-    f2.setAttribute("style", "background-image: url('IMG/" + zdjecia[(numerZdjecia)%zdjecia.length] + ".jpg')")
+    f1.setAttribute("style", "background-image: url('IMG/" + pictures[(idPicture + 1) % pictures.length] + "" + formatPicture+"')");
+    f2.setAttribute("style", "background-image: url('IMG/" + pictures[(idPicture) % pictures.length] + "" +formatPicture+"')");
     
+    idPicture2Show = ((idPicture + 1) % pictures.length)+1;
+    pictureName = pictures[(idPicture + 1) % pictures.length] + formatPicture;
+
     f2.classList.remove("hide");
     setTimeout(function () {
         f1.classList.remove("hide");
         f2.classList.add("hide"); 
     }, 500);
 
-    numerZdjecia += 1;
+    idPicture += 1;
 
-};
+    showDescription();
+}
 
 function prev() {
 
@@ -42,17 +76,21 @@ function prev() {
     f1.classList.add("slideInRight");
     f2.classList.add("slideOutLeft");
 
-    if (numerZdjecia < 0) {
-        numerZdjecia = zdjecia.length + (numerZdjecia % zdjecia.length)
+    if (idPicture < 0) {
+        idPicture = pictures.length + (idPicture % pictures.length);
     } 
 
     f1.classList.add("hide");
-    if ((numerZdjecia-1) == -1){
-        f1.setAttribute("style", "background-image: url('IMG/" + zdjecia[zdjecia.length-1] + ".jpg')")
+    if ((idPicture-1) == -1) {
+        f1.setAttribute("style", "background-image: url('IMG/" + pictures[pictures.length - 1] + "" + formatPicture+"')");
+        pictureName = pictures[pictures.length - 1] + formatPicture;
+        idPicture2Show = pictures.length - 1 + 1;
     } else {
-        f1.setAttribute("style", "background-image: url('IMG/" + zdjecia[(numerZdjecia - 1) % zdjecia.length] + ".jpg')")
+        f1.setAttribute("style", "background-image: url('IMG/" + pictures[(idPicture - 1) % pictures.length] + "" + formatPicture +"')");
+        pictureName = pictures[(idPicture - 1) % pictures.length] + formatPicture;
+        idPicture2Show = ((idPicture - 1) % pictures.length)+1;
     }
-    f2.setAttribute("style", "background-image: url('IMG/" + zdjecia[(numerZdjecia) % zdjecia.length] + ".jpg')")
+    f2.setAttribute("style", "background-image: url('IMG/" + pictures[(idPicture) % pictures.length] + "" + formatPicture +"')");
 
     f2.classList.remove("hide");
     setTimeout(function () {
@@ -61,6 +99,7 @@ function prev() {
        
     }, 500);
 
-    numerZdjecia--;
+    showDescription();
 
-};
+    idPicture--;
+}
